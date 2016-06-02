4cedb08f7dd689e2c991ebbfc6d48e3ee469b88b
rémi  <remirobert33530@gmail.com>
2016-06-02 17:49:11 +0800

    add gradient view

---
+++ FaceRecognition/GradientView.h
@@ -0,0 +1,13 @@
+//
+//  GradientView.h
+//  FaceRecognition
+//
+//  Created by Remi Robert on 02/06/16.
+//  Copyright © 2016 Remi Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GradientView : UIView
+
+@end
---
+++ FaceRecognition/GradientView.m
@@ -0,0 +1,23 @@
+//
+//  GradientView.m
+//  FaceRecognition
+//
+//  Created by Remi Robert on 02/06/16.
+//  Copyright © 2016 Remi Robert. All rights reserved.
+//
+
+#import "GradientView.h"
+
+@implementation GradientView
+
+- (void)drawRect:(CGRect)rect {
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.bounds;
+    gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor clearColor] CGColor],
+                       (id)[[[UIColor blackColor] colorWithAlphaComponent:0.5] CGColor],
+                       (id)[[UIColor blackColor] CGColor],
+                       (id)[[UIColor blackColor] CGColor], nil];
+    [self.layer insertSublayer:gradient atIndex:0];
+}
+
+@end
