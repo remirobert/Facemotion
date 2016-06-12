f9cf4dfc7e90d56dc29837f4d5f8f73ec1e8695d
rémi  <remirobert33530@gmail.com>
2016-06-12 20:36:28 +0800

    add action sheet each frame

--- FaceRecognition/ProcessingRecognitionTableViewController.mm
+++ FaceRecognition/ProcessingRecognitionTableViewController.mm
@@ -26,6 +26,30 @@
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
+- (void)startRecognition:(UIImage *)face {
+    RLMResults<FaceContact *> *contactsFace = [FaceContact allObjects];
+    NSMutableArray<FaceContact *> *contacts = [NSMutableArray new];
+    for (NSInteger index = 0; index < contactsFace.count; index++) {
+        [contacts addObject:[contactsFace objectAtIndex:index]];
+    }
+    RecognitionResult *result = [FaceRecognition recognitionFace:contacts face:face];
+    FaceContact *contact = result.contact;
+    
+    self.labelConfidence.text = [NSString stringWithFormat:@"confidence : %f", result.confidence];
+    [ContactManager fetchWithId:contact.id completion:^(ContactModel *contact) {
+        self.nameResult.text = contact.name;
+        
+        NSPredicate *pred = [NSPredicate predicateWithFormat:@"id = %@", contact.id];
+        RLMResults<FaceContact *> *facesContact = [FaceContact objectsWithPredicate:pred];
+        
+        if (facesContact.count > 0) {
+            FaceContact *firstFace = [facesContact objectAtIndex:0];
+            UIImage *image = [UIImage imageWithData:firstFace.imageData];
+            self.imageviewResult.image = image;
+        }
+    }];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tableView.delegate = self;
@@ -34,29 +58,7 @@
     self.collectionviewFrames.delegate = self;
     self.collectionviewFrames.dataSource = self;
 
-    RLMResults<FaceContact *> *contactsFace = [FaceContact allObjects];
-    NSMutableArray<FaceContact *> *contacts = [NSMutableArray new];
-    for (NSInteger index = 0; index < contactsFace.count; index++) {
-        [contacts addObject:[contactsFace objectAtIndex:index]];
-    }
-    if (contactsFace.count > 0) {
-        RecognitionResult *result = [FaceRecognition recognitionFace:contacts face:[self.face.faces firstObject]];
-        FaceContact *contact = result.contact;
-
-        self.labelConfidence.text = [NSString stringWithFormat:@"confidence : %f", result.confidence];
-        [ContactManager fetchWithId:contact.id completion:^(ContactModel *contact) {
-            self.nameResult.text = contact.name;
-            
-            NSPredicate *pred = [NSPredicate predicateWithFormat:@"id = %@", contact.id];
-            RLMResults<FaceContact *> *facesContact = [FaceContact objectsWithPredicate:pred];
-            
-            if (facesContact.count > 0) {
-                FaceContact *firstFace = [facesContact objectAtIndex:0];
-                UIImage *image = [UIImage imageWithData:firstFace.imageData];
-                self.imageviewResult.image = image;
-            }
-        }];
-    }
+    [self startRecognition:[self.face.faces firstObject]];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -69,6 +71,20 @@
     if (indexPath.section == 3) {
         [self performSegueWithIdentifier:@"selectContactSegue" sender:nil];
     }
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Recognition for this frame" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        [self startRecognition:[self.face.faces objectAtIndex:indexPath.row]];
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Remove frame" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        [self.face.faces removeObjectAtIndex:indexPath.row];
+        [self.collectionviewFrames reloadData];
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    [self presentViewController:alertController animated:true completion:nil];
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
