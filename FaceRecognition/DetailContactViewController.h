ca29fc8dd4bb72fdf70fa2f27311ff139bab8547
rémi  <remirobert33530@gmail.com>
2016-06-15 20:27:25 +0800

    add detail contact controller to parameters frames

---
+++ FaceRecognition/DetailContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailContactViewController.h
+//  FaceRecognition
+//
+//  Created by Remi Robert on 15/06/16.
+//  Copyright © 2016 Remi Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailContactViewController : UIViewController
+
+@end
---
+++ FaceRecognition/DetailContactViewController.m
@@ -0,0 +1,21 @@
+//
+//  DetailContactViewController.m
+//  FaceRecognition
+//
+//  Created by Remi Robert on 15/06/16.
+//  Copyright © 2016 Remi Robert. All rights reserved.
+//
+
+#import "DetailContactViewController.h"
+
+@interface DetailContactViewController ()
+
+@end
+
+@implementation DetailContactViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
